7c0b479f8ceb1e2a219893556799c18bbe1e0a64
Gabriel Chapron Gardes <gabrielchaprongardes@gmail.com>
2026-03-20 14:32:13 +0100

    CSS de la page index.html

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion - Inscription</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -13,10 +14,22 @@
 
     ?>
 
-    <a href="registration.php">S'inscrire</a>
-    <a href="connection.php">Se connecter</a>
-    <a href="admininscription.php">S'inscrire en tant que Chef ou Cheffe d'entreprise</a>
+    <img id="logo" src="Images/Logo.png" alt="Logo de Work4Green">
+
+    <img id="fond" src="Images/FondWork4Green.png" alt="Image de fond">
     
+    <div class="boutons">
+
+        <p>Plus de deux entreprises font une différence grâce à notre application.</p>
+
+        <a id="registration" href="registration.php">S'inscrire</a>
+
+        <a id="connection" href="connection.php">Se connecter</a>
+        
+        <a id="admininscription" href="admininscription.php">S'inscrire en tant que chef·fe d'entreprise</a>
+
+    </div>
+
 </body>
 </html>
 <?php
